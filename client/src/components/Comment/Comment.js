188e1c4b6de29c540d6c8d6c979fb3cc283deb30
Ivan Chervenkov <i.p.chervenkov@gmail.com>
2017-06-07 23:53:34 +0300

    added render reply input method

--- client/src/components/Comment/Comment.js
+++ client/src/components/Comment/Comment.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 
+import provider from '../../providers/provider';
+
 class Comment extends Component {
     constructor(props) {
         super(props);
@@ -11,10 +13,11 @@ class Comment extends Component {
             currentReply: '',
         };
         this.toogleReplies = this.toogleReplies.bind(this);
-        this.addReply = this.addReply.bind(this);
         this.renderTopLevelComment = this.renderTopLevelComment.bind(this);
         this.getRelativeTime = this.getRelativeTime.bind(this);
         this.toogleReplyInput = this.toogleReplyInput.bind(this);
+        this.handleChangeTextAreaInput = this.handleChangeTextAreaInput.bind(this);
+        this.sendReply = this.sendReply.bind(this);
     }
 
     // componentWillMount() {
@@ -56,56 +59,78 @@ class Comment extends Component {
         });
     }
 
-    addReply(ev) {
+    handleChangeTextAreaInput(ev) {
         this.setState({
             currentReply: ev.target.value
         });
     }
 
-    renderReply() {
-
+    sendReply(parentCommentId, reply) {
+        provider.addReply(parentCommentId, reply)
+            .then(() => {
+                this.setState({
+                    currentReply: '',
+                    showReplyInput: !this.state.showReplyInput
+                });
+            });
     }
 
     getRelativeTime(timestamp) {
         return moment(timestamp, 'x').fromNow();
     }
 
-    renderTopLevelComment(comment) {
-        const { showReplyInput, currentReply } = this.state;
-        const { articleId, author, createdAt, id, parentCommentId, repliesCount, text } = comment;
+    renderReply() {
+
+    }
+
+    renderReplyInput(parrentCommentId) {
+        const { currentReply } = this.state;
 
         return (
-            <div className={'comment'}>
-                <div className={'comment-header'}>
-                    <div>
-                        <span className={'comment-header__autor'}>{author}</span>
-                        <span className={'comment-header__created-at'}>{this.getRelativeTime(createdAt)}</span>
-                    </div>
-                    <div>
-                        <button className={'comment-header__toogle-replies-button'}>{`${repliesCount > 0 ? repliesCount : 'no'} replies`}</button>
-                        <button className={'comment-header__reply-button'} onClick={this.toogleReplyInput}>{`${showReplyInput ? 'discard reply' : 'reply'}`}</button>
-                    </div>
+            <div className={'reply-input-container'}>
+                <textarea cols="30" rows="8" value={currentReply} placeholder={'Body of reply'} onChange={this.handleChangeTextAreaInput} />
+                <button className={'add-reply-button'} onClick={() => this.sendReply(parrentCommentId, currentReply)}>Add Reply</button>
+            </div>
+        )
+}
+
+renderTopLevelComment(comment) {
+    const { showReplyInput } = this.state;
+    const { articleId, author, createdAt, id, parentCommentId, repliesCount, text } = comment;
+
+    return (
+        <div className={'comment'}>
+            <div className={'comment-header'}>
+                <div>
+                    <span className={'comment-header__autor'}>{author}</span>
+                    <span className={'comment-header__created-at'}>{this.getRelativeTime(createdAt)}</span>
                 </div>
-                <div className={'comment-content'}>{text}</div>
-                {showReplyInput && <div className={'reply-input-container'}><textarea cols="30" rows="8" value={currentReply} placeholder={'Body of reply'} onChange={this.addReply} /></div>}
+                <div>
+                    <button className={'comment-header__toogle-replies-button'}>{`${repliesCount > 0 ? repliesCount : 'no'} replies`}</button>
+                    <button className={'comment-header__reply-button'} onClick={this.toogleReplyInput}>{`${showReplyInput ? 'discard reply' : 'reply'}`}</button>
+                </div>
+            </div>
+            <div className={'comment-content'}>{text}</div>
+            {showReplyInput &&
+                   this.renderReplyInput(id)}
             </div>
         );
     }
 
     render() {
 
-        const { comment } = this.props;
+        const {comment} = this.props;
 
         return (
             <div className={'comment-wrapper'}>
                 {this.renderTopLevelComment(comment)}
             </div>
-        );
+            );
     }
 }
 
 Comment.propTypes = {
-    comment: PropTypes.object
+                comment: PropTypes.object
 };
 
 export default Comment;
